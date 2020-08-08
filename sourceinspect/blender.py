c8fcf50c1e8d8e1b7da3129178238dce2e3f955a
archibate <1931127624@qq.com>
2020-08-08 17:25:10 +0800

    compat with blender interactive mode

--- sourceinspect/blender.py
+++ sourceinspect/blender.py
@@ -6,6 +6,13 @@ def find_blender_source(object):
 
     old_getfile = inspect.getfile
 
+    if not hasattr(inspect.getmodule(object), '__file__'):
+        from . import CodeInspector
+        proxy = CodeInspector(object)
+        return {'_source': lambda: proxy.source,
+                '_lineno': lambda: proxy.lineno,
+                '_file': lambda: proxy.file}
+
     def new_getfile(object):
         import tempfile
         import atexit
@@ -17,7 +24,7 @@ def find_blender_source(object):
         if not is_blender:
             return ret
 
-        content = bpy.data.texts['Text'].as_string()
+        content = bpy.data.texts[ret[1:]].as_string()
         try:
             return new_getfile._si_cache[content]
         except KeyError:
@@ -29,8 +36,6 @@ def find_blender_source(object):
             f.write(content)
 
         atexit.register(os.unlink, name)
-        print('Si:', name, content)
-
         new_getfile._si_cache[content] = name
         return name
 
