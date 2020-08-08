616988067a86e6da555a5ac731a545458de5e8b4
archibate <1931127624@qq.com>
2020-08-08 17:30:21 +0800

    Add README.rst

---
+++ README.rst
@@ -0,0 +1,16 @@
+SourceInspect
+=============
+
+The functionality is mainly same as ``inspect``, but fixing the ``OSError``
+when used in some non-standard shells.
+
+Currently we support:
+
+- Python script
+- Python interactive shell
+- IPython advanced shell
+- Jupyter notebook
+- Blender scripting module
+- Blender interactive mode
+- Python IDLE file mode
+- Python IDLE interactive mode
--- setup.py
+++ setup.py
@@ -4,6 +4,20 @@ description = 'A unified inspector for retriving source from Python objects'
 long_description = '''
 SourceInspect
 =============
+
+The functionality is mainly same as ``inspect``, but fixing the ``OSError``
+when used in some non-standard shells.
+
+Currently we support:
+
+- Python script
+- Python interactive shell
+- IPython advanced shell
+- Jupyter notebook
+- Blender scripting module
+- Blender interactive mode
+- Python IDLE file mode
+- Python IDLE interactive mode
 '''
 classifiers = [
     'Intended Audience :: Developers',
