e38a1264d5efbdd8d5f0cb1cbb4ed1a95bd7a722
archibate <1931127624@qq.com>
2020-08-08 22:33:33 +0800

    imp dill.source

--- sourceinspect/dill.py
+++ sourceinspect/dill.py
@@ -5,8 +5,12 @@ class DillInspector(BasicInspector):
     def __init__(self, object):
         self.object = object
 
-        import dill.source
-        self.inspect = dill.source
+        try:
+            import dill.source
+            self.inspect = dill.source
+        except ImportError:
+            import inspect
+            self.inspect = inspect
 
     def _lineno(self):
         return self.inspect.getsourcelines(self.object)[1] + 1
